10636f3e0486bf6c9cdf88ef7dae26bb2e59161b
TornHeckr <henkdb4@pm.me>
2024-12-10 22:48:45 +0100

    Added day2.1

--- day02/01.zig
+++ day02/01.zig
@@ -2,19 +2,84 @@ const std = @import("std");
 
 const input = @embedFile("input.txt");
 
-pub fn parse(allocator: std.mem.Allocator) !std.ArrayList(std.ArrayList(u32)) {
+const Direction = enum { unknown, increasing, decreasing };
+
+const Report = struct { data: []const u8,
+
+  fn isSafe(self: *const Report) !bool {
+    const maxIncrease: i32 = 3;
+    var prev: i32 = 0;
+    var direction: Direction = .unknown;
+
+    var numbers = std.mem.split(u8, self.data, " ");
+
+    std.log.info("Validating report: {s}", .{self.data});
+
+    while (numbers.next()) | number | {
+      const parsed = try std.fmt.parseInt(i32, number, 10);
+      std.log.debug("Comparing numbers: prev{d} & parsed{}", .{prev, parsed});
+    
+      if (prev == 0) {
+        std.log.debug("Setting initial number", .{});
+        prev = parsed;
+        continue;
+      }
+
+      if (direction == .unknown) {
+        if (prev < parsed) {
+          direction = .increasing;
+          std.log.debug("Setting direction to: {any}", .{direction});
+        } else if (prev > parsed) {
+          direction = .decreasing;
+          std.log.debug("Setting direction to: {any}", .{direction});
+        } else { 
+          std.log.debug("numbers the same, so report unsafe", .{});
+          return false;
+        }
+      }
+
+      if (direction == .increasing) {
+        if (prev >= parsed) {
+          std.log.debug("Oh no, Backwards: prev {d} & parsed {d}", .{prev, parsed});
+          return false;
+        }
+
+        if (prev + maxIncrease < parsed) {
+          std.log.debug("Oh no, to high: prev {d} & parsed {d}", .{prev, parsed});
+          return false;
+        }
+      }
+
+      if (direction == .decreasing) {
+        if (prev <= parsed){
+          std.log.debug("Oh no, Backwards: prev {d} & parsed {d}", .{prev, parsed});
+          return false;
+        }
+
+        if (prev - maxIncrease > parsed){
+          std.log.debug("Oh no, to high: prev {d} & parsed {d}", .{prev, parsed});
+          return false;
+        }
+      }
+      
+      prev = parsed;
+    }
+
+    return true;
+  }
+};
+
+
+
+pub fn parse(allocator: std.mem.Allocator) !std.ArrayList(Report) {
 
   var iter = std.mem.split(u8, input, "\n");
-  
-  var reports = std.ArrayList(std.ArrayList(u32)).init(allocator);
+  var reports = std.ArrayList(Report).init(allocator);
 
   while (iter.next()) | line | {
-    var report = std.ArrayList(u32).init(allocator);
-    var numbers = std.mem.split(u8, line, " ");
+    if (line.len == 0) continue; // Last line is empty, so ignore it
 
-    while (numbers.next()) |number| {
-      try report.append(try std.fmt.parseInt(u32, number, 10));
-    }
+    const report = Report{ .data = line, }; 
 
     try reports.append(report);
   }
@@ -30,5 +95,16 @@ pub fn main() !void {
   const reports = try parse(gp.allocator());
   defer reports.deinit();
 
-  std.log.debug("reports: {any}", .{reports});
+  var safeReports: usize = 0;
+
+  for (reports.items) | report | {
+    if (try report.isSafe()) {
+      std.log.info("Report {s} is safe", .{report.data});
+      safeReports = safeReports + 1;
+    } else {
+      std.log.info("Report {s} is not safe", .{report.data});
+    }
+  }
+
+  std.log.info("Amount of safe reports: {d}", .{safeReports});
 }
